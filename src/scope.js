1c5767149280b576a909456a5f35ce253dcf91aa
Tyler Garrett <tyler@tgrrtt.com>
2014-11-16 23:25:13 -0800

    Some notes for the scope

--- src/scope.js
+++ src/scope.js
@@ -2,10 +2,13 @@
 'use strict';
 
 function initWatchVal() {}
+
 function Scope() {
   this.$$watchers = [];
 }
 
+// Takes a watchFn, which takes the scope and returns a property of that scope
+// and a listenerFn (oldVal, newVal, scope)
 Scope.prototype.$watch = function(watchFn, listenerFn) {
   var watcher = {
     watchFn: watchFn,
@@ -17,15 +20,21 @@ Scope.prototype.$watch = function(watchFn, listenerFn) {
 };
 
 
+// digest runs through all watchers
+// checks their values, and calls the listenerFn if there are changes, or its the first time run.
 Scope.prototype.$digest = function() {
   var self = this;
   var newValue, oldValue;
+
   _.forEach(this.$$watchers, function(watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
+    // dirty check
     if (newValue !== oldValue) {
       watcher.last = newValue;
       watcher.listenerFn(newValue,
+        // if the old value is init watch value, return the new value (actual orignal value)
+        // this keeps us from sending initWatchVal out of the scope
         (oldValue === initWatchVal ? newValue : oldValue),
         self);
     }
