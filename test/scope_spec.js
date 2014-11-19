bc45c0407027847f6839adac2b0fc7080ac99af4
Tyler Garrett <tyler@tgrrtt.com>
2014-11-19 14:01:01 -0800

    digest doesnt keep running when NaN

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -211,4 +211,21 @@ describe("digest", function() {
     expect(scope.counter).toBe(2);
   });
 
+  it("correctly handles NaNs", function() {
+    scope.number = 0/0;
+    scope.counter = 0;
+ 
+    scope.$watch(
+      function(scope) { return scope.number; },
+      function(newValue, oldValue, scope) {
+        scope.counter++;
+      }
+    );
+
+    scope.$digest();
+    expect(scope.counter).toBe(1);
+    scope.$digest();
+    expect(scope.counter).toBe(1);
+  });
+
 });
