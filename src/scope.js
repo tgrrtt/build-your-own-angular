904cc78b4dbe4d6ac6cf8c5ac4464b63f30ec445
Tyler Garrett <tyler@tgrrtt.com>
2014-11-18 19:51:43 -0800

    Added deep equality checking

--- src/scope.js
+++ src/scope.js
@@ -10,14 +10,16 @@ function Scope() {
 
 // Takes a watchFn, which takes the scope and returns a property of that scope
 // and a listenerFn (oldVal, newVal, scope)
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
   var watcher = {
     watchFn: watchFn,
     listenerFn: listenerFn || function() {},
+    valueEq: !!valueEq,
     last: initWatchVal
   };
 
   this.$$watchers.push(watcher);
+  this.$$lastDirtyWatch = null;
 };
 
 
@@ -31,22 +33,30 @@ Scope.prototype.$$digestOnce = function() {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
     // dirty check
-    if (newValue !== oldValue) {
+    if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
       self.$$lastDirtyWatch = watcher;
-      watcher.last = newValue;
+      // set it either to a value, or deep copy.
+      watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
       watcher.listenerFn(newValue,
         // if the old value is init watch value, return the new value (actual orignal value)
         // this keeps us from sending initWatchVal out of the scope
         (oldValue === initWatchVal ? newValue : oldValue),
         self);
+      // set dirty to true so the digest keeps checking it.
+      // I think this is the correct chain of events:
+      // if the item is initialized, the digest runs once, and sets oldvalue to initialized value
+      // it then runs once more because dirty was set to true, but then it sees that the new vaues are the same, and exits
       dirty = true;
     } else if (self.$$lastDirtyWatch === watcher) {
+      // exit it out of the loop, and return dirty, which is unefined.
       return false;
     }
   });
+
   return dirty;
 };
 
+// run the digest loop, looping until dirty isnt true, or if over 10 iterations is reached. 
 Scope.prototype.$digest = function() {
   var ttl = 10;
   var dirty;
@@ -57,4 +67,13 @@ Scope.prototype.$digest = function() {
       throw "10 digest iterations reached";
     }
   } while (dirty);
+};
+
+// use this to check equality. If looking for deep equals, it will handle it appropriately.
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+  if (valueEq) {
+    return _.isEqual(newValue, oldValue);
+  } else {
+    return newValue === oldValue;
+  }
 };
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -145,6 +145,7 @@ describe("digest", function() {
 
     expect((function(){ scope.$digest();})).toThrow();
   });
+
   it("ends the digest when the last watch is clean", function() {
 
     scope.array = _.range(100);
@@ -166,4 +167,48 @@ describe("digest", function() {
     scope.$digest();
     expect(watchExecutions).toBe(301);
   });
+
+
+  it("does not end the digest so that new watches are not run", function() {
+
+    scope.aValue = 'abc';
+    scope.counter = 0;
+
+    scope.$watch(
+      function(scope) { return scope.aValue; },
+      function(newValue, oldValue, scope) {
+        scope.$watch(
+          function(scope) {return scope.aValue; },
+          function(newValue, oldValue, scope) {
+            scope.counter++;
+          }
+        );
+      }
+    );
+
+    scope.$digest();
+    expect(scope.counter).toBe(1);
+
+  });
+
+  it("compares based on value if enabled", function() {
+    scope.aValue = [1,2,3];
+    scope.counter= 0;
+
+    scope.$watch(
+      function(scope) { return scope.aValue; },
+      function(newValue, oldValue, scope) {
+        scope.counter++;
+      },
+      true
+    );
+
+    scope.$digest();
+    expect(scope.counter).toBe(1);
+
+    scope.aValue.push(4);
+    scope.$digest();
+    expect(scope.counter).toBe(2);
+  });
+
 });
