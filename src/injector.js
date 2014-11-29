6841746fdf52448a671f13a7080f7c90e4abf15f
Tyler Garrett <tyler@tgrrtt.com>
2014-11-29 13:59:59 -0800

    Annotation strips comments and underscores

--- src/injector.js
+++ src/injector.js
@@ -5,7 +5,9 @@
 function createInjector(modulesToLoad) {
   var cache = {};
   var loadedModules = {};
-  
+  var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+  var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+  var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg;
   var $provide = {
     constant: function(key, value) {
       // prevent overriding hasOwnProperty
@@ -28,6 +30,23 @@ function createInjector(modulesToLoad) {
     });
     return fn.apply(self, args);
   }
+
+  function annotate(fn) {
+    if (_.isArray(fn)) {
+      return fn.slice(0, fn.length - 1);
+    } else if (fn.$inject) {
+      return fn.$inject;
+    } else if (!fn.length) {
+      return [];
+    } else {
+      var source = fn.toString().replace(STRIP_COMMENTS, '');
+      var argDeclaration = source.match(FN_ARGS);
+      return _.map(argDeclaration[1].split(','), function(argName) {
+        return argName.match(FN_ARG)[2];
+      });
+    }
+  }
+
   // iterate through all modules and store what its supposed to do in the cache
   _.forEach(modulesToLoad, function loadModule(moduleName) {
     var module = angular.module(moduleName);
@@ -49,6 +68,7 @@ function createInjector(modulesToLoad) {
     get: function(key) {
       return cache[key];
     },
-    invoke: invoke
+    invoke: invoke,
+    annotate: annotate
   };
 }
--- test/injector_spec.js
+++ test/injector_spec.js
@@ -137,4 +137,73 @@ describe('injector', function() {
 
     expect(injector.invoke(fn, undefined, {b: 3})).toBe(4);
   });
+
+  describe('annotate', function() {
+    it('returns the $inject annotation of a function when it has one', function() {
+      var injector = createInjector([]);
+
+      var fn = function() { };
+      fn.$inject = ['a', 'b'];
+
+      expect(injector.annotate(fn)).toEqual(['a','b']);
+    });
+
+    it('returns the array-style annotations of a function', function() {
+      var injector = createInjector([]);
+
+      var fn = ['a', 'b', function() { }];
+
+      expect(injector.annotate(fn)).toEqual(['a', 'b']);
+    });
+
+    it('returns nan empty array for a non-annotated 0-arg function', function() {
+      var injector = createInjector([]);
+      var fn = function() { };
+
+      expect(injector.annotate(fn)).toEqual([]);
+    });
+
+    it('returns annotations parsed from function args when not annotated', function() {
+      var injector = createInjector([]);
+
+      var fn = function(a, b) { };
+
+      expect(injector.annotate(fn)).toEqual(['a','b']);
+    });
+
+    it('strips comments from argument lists when parsing', function() {
+      var injector = createInjector([]);
+      var fn = function(a, /*b, */ c) { };
+
+      expect(injector.annotate(fn)).toEqual(['a', 'c']);
+
+    });
+
+    it('strips severalcomments from arguments lists when parsing', function() {
+      var injector = createInjector([]);
+
+      var fn = function(a, /*b,*/ c/*, d*/) { };
+
+      expect(injector.annotate(fn)).toEqual(['a','c']);
+
+    });
+
+    it('strips out // when parsing', function() {
+      var injector = createInjector([]);
+
+      var fn = function(a, //b,
+          c) { };
+
+      expect(injector.annotate(fn)).toEqual(['a', 'c']);
+
+    });
+
+    it('strips surrounding underscores from argument names when parsing', function() {
+      var injector = createInjector([]);
+
+      var fn = function(a, _b_, c_, _d, an_argument) { };
+
+      expect(injector.annotate(fn)).toEqual(['a', 'b', 'c_', '_d', 'an_argument']);
+    });
+  });
 });
